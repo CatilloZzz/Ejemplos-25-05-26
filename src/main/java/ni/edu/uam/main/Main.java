bb6606288021f86fcaf585c4316e50d81fc108e4
CatilloZzz <jireyes@uamv.edu.ni>
2026-05-25 15:43:58 -0600

    feat: Se agregaron productos

--- src/main/java/ni/edu/uam/main/Main.java
+++ src/main/java/ni/edu/uam/main/Main.java
@@ -1,12 +1,14 @@
 package ni.edu.uam.main;
 
+import ni.edu.uam.modelos.Producto;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        Set<String> productos = new HashSet<>();
+      /*  Set<String> productos = new HashSet<>();
 
         productos.add("mouse");
         productos.add("auriculares");
@@ -16,7 +18,20 @@ public class Main {
          for (String p : productos){
              System.out.println(p);
 
-         }
+         }*/
 
-    }
+        Set<Producto> catalogo = new HashSet<>();
+        Producto cafe = new Producto("presto Libra", 350, 50  );
+        Producto teclado = new Producto("Teclado Logitech", 500, 60  );
+        Producto camara = new Producto("Camara sony", 30000, 4  );
+
+        catalogo.add(cafe);
+        catalogo.add(teclado);
+        catalogo.add(camara);
+
+        for (Producto p : catalogo) {
+            System.out.println(p);
+
+
+        }   }
 }
