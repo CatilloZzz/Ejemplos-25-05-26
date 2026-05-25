821e34f2189553dfdb5e5c88b04720d451dba342
CatilloZzz <jireyes@uamv.edu.ni>
2026-05-25 15:50:52 -0600

    feat: se agregaron 7 productos

--- src/main/java/ni/edu/uam/main/Main.java
+++ src/main/java/ni/edu/uam/main/Main.java
@@ -24,10 +24,27 @@ public class Main {
         Producto cafe = new Producto("presto Libra", 350, 50  );
         Producto teclado = new Producto("Teclado Logitech", 500, 60  );
         Producto camara = new Producto("Camara sony", 30000, 4  );
+       /************************************************************************************/
+        Producto Laptop = new Producto("Lenovo Thinkpad", 30000, 10  );
+        Producto celular = new Producto("Samsung S24 Ultra ", 25000, 15  );
+        Producto componentes = new Producto("Grafica  5060 ", 45000, 10);
+        Producto tablet = new Producto("iPad Pro 11", 18000, 20);
+        Producto auriculares = new Producto("Sony Ericzon", 6500, 30);
+        Producto reloj = new Producto("Steam Deck", 12000, 12);
+        Producto consola = new Producto("PlayStation 5", 10500, 25);
+
 
         catalogo.add(cafe);
         catalogo.add(teclado);
         catalogo.add(camara);
+        /************************/
+        catalogo.add(Laptop);
+        catalogo.add(celular);
+        catalogo.add(componentes);
+        catalogo.add(tablet);
+        catalogo.add(auriculares);
+        catalogo.add(reloj);
+        catalogo.add(consola);
 
         for (Producto p : catalogo) {
             System.out.println(p);
